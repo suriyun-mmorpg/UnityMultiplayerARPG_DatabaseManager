0d7cf0177f9b2869089af798f67af090497bfaf8
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-02-23 03:14:23 +0700

    feat: add broken script fixing codes

---
+++ SharedDataFix/Scripts/CharacterData/CharacterData.Fix.cs
@@ -0,0 +1,18 @@
+﻿namespace MultiplayerARPG
+{
+    public partial class CharacterData
+    {
+        public void FillWeaponSetsIfNeeded(byte equipWeaponSet)
+        {
+            while (SelectableWeaponSets.Count <= equipWeaponSet)
+            {
+                SelectableWeaponSets.Add(new EquipWeapons());
+            }
+        }
+
+        public void MarkToMakeCaches()
+        {
+            // Do nothing
+        }
+    }
+}
