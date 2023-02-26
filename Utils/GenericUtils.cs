0747b21f2b010af2a63e4d39c7060d6f6c6f5a78
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-02-26 18:04:35 +0700

    feat: add ID/Password functions

---
+++ Utils/GenericUtils.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MultiplayerARPG
+{
+    public static class GenericUtils
+    {
+        public static string GetUniqueId(int length = 16, string mask = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_-")
+        {
+            return Nanoid.Nanoid.Generate(mask, length);
+        }
+
+        public static string GetMD5(this string text)
+        {
+            // byte array representation of that string
+            byte[] encodedPassword = new UTF8Encoding().GetBytes(text);
+
+            // need MD5 to calculate the hash
+            byte[] hash = ((HashAlgorithm)CryptoConfig.CreateFromName("MD5")).ComputeHash(encodedPassword);
+
+            // string representation (similar to UNIX format)
+            return System.BitConverter.ToString(hash).Replace("-", string.Empty).ToLower();
+        }
+    }
+}
---
+++ Utils/PasswordHashing.cs
@@ -0,0 +1,22 @@
+﻿public static class StringUtils
+{
+    public delegate string HashDelegate(string password);
+    public delegate bool VerifyDelegate(string password, string hashedPassword);
+
+    public static HashDelegate overrideHash;
+    public static VerifyDelegate overrideVerify;
+
+    public static string PasswordHash(this string password)
+    {
+        if (overrideHash != null)
+            return overrideHash.Invoke(password);
+        return password.GetMD5();
+    }
+
+    public static bool PasswordVerify(this string password, string hashedPassword)
+    {
+        if (overrideVerify != null)
+            return overrideVerify.Invoke(password, hashedPassword);
+        return password.GetMD5().Equals(hashedPassword);
+    }
+}
