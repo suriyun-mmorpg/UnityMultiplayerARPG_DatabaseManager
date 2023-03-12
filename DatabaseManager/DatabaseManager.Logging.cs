3f2e94dc3ece227d2e8ad13b7004fed25ec395bc
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-03-13 05:35:36 +0700

    Implement shared codes interfaces

---
+++ DatabaseManager/DatabaseManager.Logging.cs
@@ -0,0 +1,27 @@
+﻿namespace MultiplayerARPG.MMO
+{
+    public partial class BaseDatabase
+    {
+        private ILogger logger;
+
+        public void LogInformation(string tag, string msg)
+        {
+            logger.LogInformation(msg);
+        }
+
+        public void LogWarning(string tag, string msg)
+        {
+            logger.LogWarning(msg);
+        }
+
+        public void LogError(string tag, string msg)
+        {
+            logger.LogError(msg);
+        }
+
+        public void LogException(string tag, System.Exception ex)
+        {
+            logger.LogCritical(ex, string.Empty);
+        }
+    }
+}
