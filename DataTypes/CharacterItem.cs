4b4e99d1c34cc3d084973f100ba842afd22bd573
insthync <ittipon.bay@gmail.com>
2024-04-02 11:26:44 +0700

    improve: change condition to detect empty slot

---
+++ DataTypes/CharacterItem.cs
@@ -0,0 +1,20 @@
+﻿namespace MultiplayerARPG
+{
+    public partial struct CharacterItem
+    {
+        public bool IsEmpty()
+        {
+            return Equals(Empty);
+        }
+
+        public bool IsEmptySlot()
+        {
+            return IsEmpty() || dataId == 0 || amount <= 0;
+        }
+
+        public bool NotEmptySlot()
+        {
+            return !IsEmptySlot();
+        }
+    }
+}
