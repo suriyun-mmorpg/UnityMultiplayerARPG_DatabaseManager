dcb3359eed18c9e0ef2602a8f576c38347e92ead
insthync <ittipon.bay@gmail.com>
2024-08-21 13:22:41 +0700

    feat: add postgresql constructor

---
+++ DatabaseManager/PostgreSQLDatabase.Implements.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiplayerARPG.MMO
+{
+    public partial class PostgreSQLDatabase
+    {
+        public PostgreSQLDatabase(ILogger<PostgreSQLDatabase> logger, IDatabaseUserLogin userLoginManager) : base(logger, userLoginManager)
+        {
+        }
+    }
+}
